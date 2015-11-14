74c98bc8e68fe636aff55647ddcb868971d8046a
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-11-14 17:34:08 -0500

    writing python variable to html page

--- CREDITS.md
+++ CREDITS.md
@@ -13,7 +13,7 @@
  + http://georgik.sinusgear.com/2011/01/07/how-to-dump-post-request-with-python/
  + [Simple python web server to demonstrate GET/POST handling](http://joelinoff.com/blog/?p=1658)
  + http://stackoverflow.com/questions/12731207/how-can-i-debug-post-requests-with-pythons-basehttpserver-simplehttpserver
- + http://www.cs.sfu.ca/CourseCentral/165/common/guide/html/sec-cgi.html
+ + [Print HTML body using form data](http://www.cs.sfu.ca/CourseCentral/165/common/guide/html/sec-cgi.html)
  + https://snipt.net/raw/f8ef141069c3e7ac7e0134c6b58c25bf/?nice
  + [writing posted data to file](http://stackoverflow.com/a/25361432/670433)
  + [forwarding with python simpleserver](http://stackoverflow.com/questions/2506932/how-do-i-forward-a-request-to-a-different-url-in-python)
@@ -21,3 +21,7 @@
  + https://github.com/PyMySQL/PyMySQL#example
  + https://docs.python.org/2/tutorial/errors.html
  + https://github.com/gwu-business/istm-4121/blob/394ad2f9d6f5de8b626cc6d81d57a545b342ba3d/notes/database-management/database-management-sql.md
+ + [Python DOM Manipulation](https://wiki.python.org/moin/MiniDom)
+ + [Adding a Node to a DOM Tree Tutorial](http://www.java2s.com/Tutorial/Python/0400__XML/AddingaNodetoaDOMTree.htm)
+ + https://www.quora.com/What-is-an-easy-to-use-HTML-XML-parser-generator-library-in-Python-similar-to-Hpricot-Nokogiri-in-Ruby
+ + http://www.crummy.com/software/BeautifulSoup/bs4/doc/
--- software/start_local_web_server.py
+++ software/start_local_web_server.py
@@ -6,6 +6,8 @@
 #   + http://georgik.sinusgear.com/2011/01/07/how-to-dump-post-request-with-python/
 #   + https://snipt.net/raw/f8ef141069c3e7ac7e0134c6b58c25bf/?nice
 #   + https://github.com/PyMySQL/PyMySQL#example
+#   + http://www.cs.sfu.ca/CourseCentral/165/common/guide/html/sec-cgi.html
+#   + https://wiki.python.org/moin/BaseHttpServer
 
 import code # to debug: `code.interact(local=locals())`
 import logging # to log: `logging.warning("MY MESSAGE")` or `logging.error("MY MESSAGE")`
@@ -22,11 +24,77 @@ PORT = 8818
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+    # OVERWRITE BEHAVIOR OF "GET" REQUESTS
+
     def do_GET(self):
         if ".html" in self.path: # only log messages for html pages, not images and scripts
             self.log_message("GETTING: " + self.path)
             self.log_message("HEADERS: " + json.dumps(dict(self.headers)))
+
+            # IF GETTING THE MENU PATH, READ MENU ITEMS FROM DATABASE
+
+            #if self.path == "/menu-items/index.html":
+            if self.path == "/menu-items/show.html":
+                self.log_message("QUERYING THE DATABASE")
+                menu_items = [
+                  {"id":1, "title":"first salad", "description": "a salad"},
+                  {"id":2, "title":"second salad", "description": "a salad"},
+                  {"id":3, "title":"third salad", "description": "a salad"}
+                ]
+
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write("<html><head><title>Title goes here.</title></head>")
+                self.wfile.write("<body><p>This is a test.</p>")
+                # If someone went to "http://something.somewhere.net/foo/bar/",
+                # then s.path equals "/foo/bar/".
+                self.wfile.write("<p>You accessed path: %s</p>" % self.path)
+                self.wfile.write("</body></html>")
+
+
+
+
+
+
+                #### ESTABLISH DATABASE CONNECTION
+
+                ###connection = pymysql.connect(
+                ###    host='localhost',
+                ###    port=3306,
+                ###    user='root',
+                ###    passwd='y0l0', # or change, or leave blank or comment-out
+                ###    db='salad_db',
+                ###    #charset='utf8mb4',
+                ###    cursorclass=pymysql.cursors.DictCursor
+                ###)
+
+                #### EXECUTE DATABASE TRANSACTION
+
+                ###try:
+
+                ###    # PRINT RECORDS
+
+                ###    with connection.cursor() as cursor:
+                ###        sql = "SELECT * FROM menu_items ORDER BY id DESC LIMIT 1"
+                ###        cursor.execute(sql)
+                ###        result = cursor.fetchone()
+                ###        print(result)
+
+                ###finally:
+                ###    connection.close() # for performance
+
+
+
+
+
+
+
+
+
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    # OVERWRITE BEHAVIOR OF "POST" REQUESTS
 
     def do_POST(self):
         self.log_message("POSTING: " + self.path)
