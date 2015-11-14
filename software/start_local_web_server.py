90cc6c79fe30f102b271b93f5a40221288d26d8a
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-11-13 23:03:01 -0500

    storing form data in the database

--- software/start_local_web_server.py
+++ software/start_local_web_server.py
@@ -1,8 +1,11 @@
-# a python implementation of a web server which recognizes data from post requests
-# to run: `python salad_server.py`
+# a python implementation of a local web server which
+# ... recognizes web form data from post requests
+# ... and stores web form data in a mysql database.
+# to run from root dir: `python software/start_local_web_server.py`
 # source(s):
 #   + http://georgik.sinusgear.com/2011/01/07/how-to-dump-post-request-with-python/
 #   + https://snipt.net/raw/f8ef141069c3e7ac7e0134c6b58c25bf/?nice
+#   + https://github.com/PyMySQL/PyMySQL#example
 
 import code # to debug: `code.interact(local=locals())`
 import logging # to log: `logging.warning("MY MESSAGE")` or `logging.error("MY MESSAGE")`
@@ -11,8 +14,11 @@ import SocketServer
 import cgi
 import json
 #import urlparse
+import pymysql.cursors
 
 PORT = 8818
+
+# DEFINE THE LOCAL WEB SERVER
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
@@ -26,7 +32,8 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.log_message("POSTING: " + self.path)
         self.log_message("HEADERS: " + json.dumps(dict(self.headers)))
 
-        # READ FORM DATA ...
+        # READ FORM DATA
+
         form = cgi.FieldStorage(
             fp=self.rfile,
             headers=self.headers,
@@ -36,32 +43,74 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             }
         )
 
-        # TRANSFORM FORM DATA ...
+        # LOG FORM DATA
+
         form_dict = {}
         for attribute in form.list:
             form_dict[attribute.name] = attribute.value
         self.log_message("POSTED DATA: " + json.dumps(form_dict))
 
-        # STORE FORM DATA IN DATABASE ...
+        # IF POSTING TO THE NEW MENU ITEMS PATH, CREATE A NEW MENU ITEM RECORD IN THE DATABASE
+
         if self.path == "/menu-items/new.html":
             self.log_message("STORING: " + json.dumps(form_dict))
 
+            # TRANSFORM DATA
 
-            
+            category = "SpecialSalad"
             title = form['title'].value
-            calories = form['calories'].value # int(form['calories'].value)
+            calories = form['calories'].value
+            calories = int(calories)
+            contains_gluten = True
+            vegan_safe = False
             description = form['description'].value
 
+            # ESTABLISH DATABASE CONNECTION
 
-            #TODO: db insert statement
+            connection = pymysql.connect(
+                host='localhost',
+                port=3306,
+                user='root',
+                passwd='y0l0', # or change, or leave blank or comment-out
+                db='salad_db',
+                #charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
+            # EXECUTE DATABASE TRANSACTION
+
+            try:
+
+                # CREATE NEW RECORD
+
+                with connection.cursor() as cursor:
+                    sql = "INSERT INTO `menu_items` (`category`,`title`,`calories`,`contains_gluten`,`vegan_safe`,`description`) VALUES (%s, %s, %s, %s, %s, %s)"
+                    cursor.execute(sql, (category, title, calories, contains_gluten, vegan_safe, description)  )
+                connection.commit() # to save the changes
+
+                # PRINT NEW RECORD
+
+                with connection.cursor() as cursor:
+                    sql = "SELECT * FROM menu_items ORDER BY id DESC LIMIT 1"
+                    cursor.execute(sql)
+                    result = cursor.fetchone()
+                    print(result)
+
+            finally:
+                connection.close() # for performance
+
             self.log_message("STORED")
+
             # REDIRECT TO MENU INDEX
+
             self.log_message("REDIRECTING")
             self.send_response(301)
             self.send_header('Location',"/menu-items/index.html")
             self.end_headers()
 
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+# RUN THE LOCAL WEB SERVER
 
 Handler = ServerHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
